063fb8c08f7562d1573b246cf487c04077a15e0c
NSDmitry <idmitryserov@gmail.com>
2017-12-11 00:01:50 +0400

    init task3 project

---
+++ Task3/Task3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task3
+//
+//  Created by D. Serov on 11/12/2017.
+//  Copyright © 2017 Dmitry Serov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
