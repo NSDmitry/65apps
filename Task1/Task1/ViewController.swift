08fe52ee2aa4cbe4fae3b6312ae2c8ae65d31817
NSDmitry <idmitryserov@gmail.com>
2017-12-10 22:43:59 +0400

    init task1 project

---
+++ Task1/Task1/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by D. Serov on 10/12/2017.
+//  Copyright © 2017 Dmitry Serov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
