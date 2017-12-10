b186e89864e8eaa7dfa953eb97486205045f25ca
NSDmitry <idmitryserov@gmail.com>
2017-12-11 02:48:48 +0400

    add validater

--- task4/task4/ViewController.swift
+++ task4/task4/ViewController.swift
@@ -12,14 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let username = "Dmitry"
+        let validater = Validater(username: username)
+        validater.valid().status
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
